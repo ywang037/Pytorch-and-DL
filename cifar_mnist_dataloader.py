aceeb81c056131ff19a3c1c990564efe26e92f2c
Wang Yuan <32762885+ywang037@users.noreply.github.com>
2021-05-12 12:29:51 +0800

    Update cifar_mnist_dataloader.py

--- cifar_mnist_dataloader.py
+++ cifar_mnist_dataloader.py
@@ -14,56 +14,65 @@ def data_cifar(path, batch_size=100):
     """
     returns training data loader and test data loader
     """
-    # no brainer normalization used in the pytorch tutorial
-    mean_0 = (0.5, 0.5, 0.5)
-    std_0 = (0.5, 0.5, 0.5)
+    # # no brainer normalization used in the pytorch tutorial
+    # mean_0 = (0.5, 0.5, 0.5)
+    # std_0 = (0.5, 0.5, 0.5)
 
+    # # configure tranform for training data
+    # # standard transform used in the pytorch tutorial 
+    # transform_train_0 = transforms.Compose([
+    #     transforms.ToTensor(),
+    #     transforms.Normalize(mean_0,std_0),
+    # ])
+
+    # # configure transform for test data
+    # transform_test = transforms.Compose([
+    #     transforms.ToTensor(),
+    #     transforms.Normalize(mean_0,std_0),
+    # ])
+    
     # alternative normilzation
     mean_1 = (0.4914, 0.4822, 0.4465)
     std_1 = (0.2023, 0.1994, 0.2010)
 
-    # configure tranform for training data
-    # standard transform used in the pytorch tutorial 
-    transform_train_0 = transforms.Compose([
-        transforms.ToTensor(),
-        transforms.Normalize(mean_0,std_0),
-    ])
-
-    # configure transform for test data
-    transform_test = transforms.Compose([
-        transforms.ToTensor(),
-        transforms.Normalize(mean_0,std_0),
-    ])
-    
-    '''
     # Alternative transform
     # enhanced transform, random crop and flip is optional
-    transform_train_1 = transforms.Compose([
+    transform_train = transforms.Compose([
         transforms.RandomCrop(32, padding=4),
         transforms.RandomHorizontalFlip(),
         transforms.ToTensor(),
         transforms.Normalize(mean_1,std_1),
     ])
 
-    # alternative, only random crop is used
-    transform_train_2 = transforms.Compose([
-        transforms.RandomCrop(32, padding=4),
-        transforms.ToTensor(),
-        transforms.Normalize(mean_1,std_1),
-    ])
+    # # Alternative transform 2, random contrast brightness, whitenning is applied
+    # transform_train = transforms.Compose([
+    #     transforms.RandomCrop(32, padding=4),
+    #     transforms.ColorJitter(brightness=0.2,contrast=0.2,saturation=0.2),
+    #     transforms.ToTensor(),
+    #     transforms.Normalize(mean_1,std_1),
+    # ])
+
+    # # Alternative transform 3, random contrast brightness, whitenning is applied
+    # # cropped image has size 24 x 24, which requires the model input size to change accordingly 
+    # transform_train = transforms.Compose([
+    #     transforms.RandomCrop(24),
+    #     transforms.ColorJitter(brightness=0.2,contrast=0.2,saturation=0.2),
+    #     transforms.ToTensor(),
+    #     transforms.Normalize(mean_1,std_1),
+    # ])
 
     # configure transform for test data
-    transform_test_1 = transforms.Compose([
+    transform_test = transforms.Compose([
         transforms.ToTensor(),
         transforms.Normalize(mean_1,std_1),
     ])
-    '''
+    
     # setup the CIFAR10 training dataset
-    data_train = datasets.CIFAR10(root=path, train=True, download=False, transform=transform_train_0)
+    data_train = datasets.CIFAR10(root=path, train=True, download=False, transform=transform_train)
     loader_train = data.DataLoader(data_train, batch_size=batch_size, shuffle=True)
 
     # setup the CIFAR10 test dataset
-    data_test = datasets.CIFAR10(root=path, train=False, download=False, transform=transform_test_0)
+    data_test = datasets.CIFAR10(root=path, train=False, download=False, transform=transform_test)
     loader_test = data.DataLoader(data_test, batch_size=100, shuffle=False)
 
     return loader_train, loader_test
